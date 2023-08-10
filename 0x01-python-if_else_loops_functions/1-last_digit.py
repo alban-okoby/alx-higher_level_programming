bdca989505e054de57a7a6cf1fd3ebe59781b3ce
Cerveau++ <77627170+alban-okoby@users.noreply.github.com>
2023-08-10 00:31:57 +0000

    Fix issue of task 2 ✅

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ last_digit = abs(number) % 10
 oderRange = ""
 
 if (last_digit > 5):
-    oderRange = "greater than 5"
+    oderRange = "greater than 5 and is not 0"
 elif (last_digit == 0):
     oderRange = "0"
 else:
     oderRange = "less than 6 and not 0"
 
-print (f"Last digit of {number} is {last_digit} and is {oderRange}")
+print(f"Last digit of {number} is {last_digit} and is {oderRange}")

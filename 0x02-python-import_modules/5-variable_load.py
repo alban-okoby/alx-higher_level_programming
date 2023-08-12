e406345225ff2d65785efcc235f0e287c8647ba8
alban-okoby <albanokoby225@gmail.com>
2023-08-11 19:20:39 -0700

    Issu Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    form variable_load_5 import a
+    """The value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
     print(a)

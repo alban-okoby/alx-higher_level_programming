8611c1f10ab52d99a2043757ff1c71688e942497
alban-okoby <albanokoby225@gmail.com>
2023-08-13 06:20:37 -0700

    Issue of Reversed list, 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     my_reverse_list = my_list.reverse()
-    for item in reversed(my_reverse_list):
+    for item in my_reverse_list:
         print("{:d}".format(item))

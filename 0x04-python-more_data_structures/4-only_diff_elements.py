ee8a7817b125ac67f15028fa8deb5dea63b531cb
alban-okoby <albanokoby225@gmail.com>
2023-08-16 17:28:16 -0700

    Issue of Only differents ❌

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,7 +6,6 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         if i not in set_2:
             diff_elements.add(i)
-    
     for j in set_2:
         if j not in set_1:
             diff_elements.add(j)

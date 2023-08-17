1d0adca85ce22eb3ebeab150c1bee462862c24c8
alban-okoby <albanokoby225@gmail.com>
2023-08-16 17:26:31 -0700

    Only differents

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    diff_elements = set()
+
+    for i in set_1:
+        if i not in set_2:
+            diff_elements.add(i)
+    
+    for j in set_2:
+        if j not in set_1:
+            diff_elements.add(j)
+    return diff_elements

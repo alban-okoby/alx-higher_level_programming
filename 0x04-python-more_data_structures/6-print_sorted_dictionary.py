6c68533c1bd66f1c88970f7bcd51dec549f727d8
alban-okoby <albanokoby225@gmail.com>
2023-08-16 17:46:18 -0700

    sorted dico

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,5 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary.keys())
 
-    for k in sorted_keys:
-        print(f"{k}: {a_dictionary[k]}")
+    for key in sorted_keys:
+        print(f"{key}: {a_dictionary[key]}")

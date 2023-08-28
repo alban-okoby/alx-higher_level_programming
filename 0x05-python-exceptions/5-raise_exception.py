37f259b34009d32928c241c7136376e4f3a74f71
alban-okoby <albanokoby225@gmail.com>
2023-08-28 15:46:53 -0700

    Issu of raise

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -2,6 +2,6 @@
 
 def raise_exception():
     try:
-        raise TypeError("A custom type exception")
+        raise TypeError
     except TypeError as error:
         print("Exception raised: ", error)

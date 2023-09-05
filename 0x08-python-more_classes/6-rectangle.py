cedcf1974db87ec630f184404bae5f41f53b1fe8
alban-okoby <albanokoby225@gmail.com>
2023-09-05 08:18:23 -0700

    Howmany instances 🙄

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+""" Class Rectangle"""
+
+
+class Rectangle:
+    """A class defining a rectangle by its width and height"""
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """Initializes a new Rectangle instance"""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Get the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Set the width of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """t the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Calculate the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculate the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Generate a string representation of the rectangle using '#' characters"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(['#' * self.__width for _ in range(self.__height)])
+
+    def __repr__(self):
+        """Generate a string representation for recreating the rectangle"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Close program"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1

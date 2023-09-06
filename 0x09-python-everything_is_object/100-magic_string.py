cc07d69ff0806f37e2e032f065fc566a8f60bd40
alban-okoby <albanokoby225@gmail.com>
2023-09-05 18:01:45 -0700

    Magic string issue ❌

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-"""A magic method"""
 def magic_string():
     magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return "BestSchool" + (", BestSchool" * (magic_string.n - 1))
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")

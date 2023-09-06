6704dca42d8f26a626ce7c85300b45b28e8da25e
alban-okoby <albanokoby225@gmail.com>
2023-09-06 04:18:40 -0700

    Fixe issue of how many instances ❌

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -54,7 +54,7 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Generate a string representation of the rectangle using '#' characters"""
+        """A string representation of the rectangle using '#' characters"""
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(['#' * self.__width for _ in range(self.__height)])

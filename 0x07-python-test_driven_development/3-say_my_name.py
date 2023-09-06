c1fcbf2a9abd2dc815456c6a5100d1b014466b1c
alban-okoby <albanokoby225@gmail.com>
2023-09-06 08:14:27 -0700

    Print a square

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
     # Check if first_name and last_name are strings
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or last_name must be a string")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
 
     # Print the name
     if last_name:

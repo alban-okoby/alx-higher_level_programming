c8a2c24b37255c65c763c6fe778b9a444a31180f
alban-okoby <albanokoby225@gmail.com>
2023-09-11 07:07:29 -0700

    Issue of 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
 """Define a List class"""
 
 
 class MyList(list):
-    """ A MyList class"""
+    """ A subclass of list"""
+    def __init__(self):
+        """Initializes of list"""
+        super().__init__()
+
     def print_sorted(self):
         """print a sorted of list(ascending order)"""
         ascended_list = sorted(self)

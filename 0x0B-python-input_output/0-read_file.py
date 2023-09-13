e7b12fed36cfb4f1554c48468e5ba6b582aaf642
alban-okoby <albanokoby225@gmail.com>
2023-09-12 17:22:58 -0700

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Define a read_file function"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints its contents to stdout
+
+    Args:
+        filename (str): The name of the text file to read
+
+    Returns:
+        None
+    """
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line, end='')
+    except FileNotFoundError:
+        pass

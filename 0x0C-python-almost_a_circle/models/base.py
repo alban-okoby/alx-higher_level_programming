4537d41f1f702425f2829045d9df5711dc9801e4
alban-okoby <albanokoby225@gmail.com>
2023-09-17 14:04:01 -0700

    Advanced JSOn Ok, CSV

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 """Define a Base class"""
 
+import csv
 
 class Base:
     """
-    Base class for managing unique IDs in derived classes
+    Base class represents the base of all classes.
 
     Attributes:
-        __nb_objects (int): A private class attribute to
-        keep track of the number of instances
-        id (int): A public instance attribute representing
-        the unique ID of an object
-
-    Methods:
-        __init__(self, id=None): Constructor for the Base class
+        __nb_objects (int): The number of objects created.
     """
 
     __nb_objects = 0
@@ -23,10 +18,7 @@ class Base:
         Initializes a new instance of the Base class.
 
         Args:
-            id (int, optional): An optional integer ID. If provided,
-            it sets the object's ID to the given value.
-                If not provided, it increments the __nb_objects counter
-                and assigns a unique ID.
+            id (int, optional): An optional integer ID (default is None).
 
         Returns:
             None
@@ -55,10 +47,11 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """
-        Save a list of instances to a JSON file.
+        Save a list of instances to a JSON file
 
         Args:
-            list_objs (list of Base instances): List of instances to be saved
+            list_objs (list of Base instances): A list of
+            instances to be saved
 
         Returns:
             None
@@ -71,3 +64,109 @@ class Base:
             obj_dicts = [obj.to_dictionary() for obj in list_objs]
             json_string = cls.to_json_string(obj_dicts)
             file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Convert a JSON string to a list of dictionaries.
+
+        Args:
+            json_string (str): A JSON string representing a
+            list of dictionaries.
+
+        Returns:
+            list: A list of dictionaries.
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Create an instance with attributes already
+        set using a dictionary
+
+        Args:
+            dictionary (dict): A dictionary containing
+            attribute values
+
+        Returns:
+            Base: An instance with attributes set from the dictionary
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = cls()
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Load a list of instances from a JSON file.
+
+        Returns:
+            list: A list of instances created from the file data.
+        """
+        filename = f"{cls.__name__}.json"
+        try:
+            with open(filename, 'r') as file:
+                json_data = file.read()
+                obj_dicts = cls.from_json_string(json_data)
+                return [cls.create(**obj_dict) for obj_dict in obj_dicts]
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        Save a list of instances to a CSV file.
+
+        Args:
+            list_objs (list of Base instances): A list of
+            instances to be saved.
+
+        Returns:
+            None
+        """
+        if list_objs is None:
+            list_objs = []
+
+        filename = f"{cls.__name__}.csv"
+        with open(filename, 'w', newline='') as file:
+            writer = csv.writer(file)
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    data = [obj.id, obj.width, obj.height, obj.x, obj.y]
+                elif cls.__name__ == "Square":
+                    data = [obj.id, obj.size, obj.x, obj.y]
+                writer.writerow(data)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        Load a list of instances from a CSV file.
+
+        Returns:
+            list: A list of instances created from the file data.
+        """
+        filename = f"{cls.__name__}.csv"
+        try:
+            with open(filename, 'r', newline='') as file:
+                reader = csv.reader(file)
+                instances = []
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        id, width, height, x, y = map(int, row)
+                        instance = cls(width, height, x, y, id)
+                    elif cls.__name__ == "Square":
+                        id, size, x, y = map(int, row)
+                        instance = cls(size, x, y, id)
+                    instances.append(instance)
+                return instances
+        except FileNotFoundError:
+            return []

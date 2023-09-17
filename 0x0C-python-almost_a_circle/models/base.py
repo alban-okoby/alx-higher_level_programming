0593a83938aebf44594126286e472aae1ab8856f
alban-okoby <albanokoby225@gmail.com>
2023-09-17 12:32:02 -0700

    Dctionnary to JSOn string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,3 +36,38 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Return the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list of dict): A list of dictionaries.
+
+        Returns:
+            str: The JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Save a list of instances to a JSON file.
+
+        Args:
+            list_objs (list of Base instances): List of instances to be saved
+
+        Returns:
+            None
+        """
+        if list_objs is None:
+            list_objs = []
+
+        filename = f"{cls.__name__}.json"
+        with open(filename, 'w') as file:
+            obj_dicts = [obj.to_dictionary() for obj in list_objs]
+            json_string = cls.to_json_string(obj_dicts)
+            file.write(json_string)

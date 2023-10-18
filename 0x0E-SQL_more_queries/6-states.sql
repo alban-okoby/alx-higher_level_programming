5f7fbb6c1f7415f5de98c302623266ebfd3588dd
alban-okoby <albanokoby225@gmail.com>
2023-10-18 15:01:05 -0700

    issue of 6 & 7

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
 -- Create the database hbtn_0d_usa if it doesn't exist.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa:
+
 -- Create the table states if it doesn't exist.
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
@@ -8,5 +10,3 @@ CREATE TABLE IF NOT EXISTS states (
     PRIMARY KEY (id),
     UNIQUE (id)
 );
-
-EXIT;

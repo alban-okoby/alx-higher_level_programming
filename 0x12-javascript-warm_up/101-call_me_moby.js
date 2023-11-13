88f9fb78a514b5b34551399a2918b0b4dfd63e44
alban-okoby <albanokoby225@gmail.com>
2023-11-13 14:18:30 -0800

    ❌Issue of callMeBoy

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
 
 // Define a function to execute another function x times
-const executeXTimes = (x, theFunction) => {
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
 };
-
-// Example usage
-const myFunction = () => {
-  console.log('Executing the function!');
-};
-
-executeXTimes(3, myFunction);
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-/**
- * Define a function to return the addition of two integers
- */
+// Define a function to return the addition of two integers
 exports.add = function (a, b) {
 	return (a + b);
 };

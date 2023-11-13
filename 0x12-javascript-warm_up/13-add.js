d689e40a294cfcfb19801c305b546e161d22e157
alban-okoby <albanokoby225@gmail.com>
2023-11-13 11:51:25 -0800

    Issu of 13

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -3,8 +3,6 @@
 /**
  * Define a function to return the addition of two integers
  */
-const add = (a, b) => a + b;
-
-// Example usage
-const result = add(3, 5);
-console.log(result);
+exports.add = function (a, b) {
+	return (a + b);
+};

356512b2c9b7bb2abf42c89a2e519275f4e715f2
alban-okoby <albanokoby225@gmail.com>
2023-11-13 11:43:05 -0800

    Issu of add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
 const add = (a, b) => {
-  return a + b;
+  const result = a + b;
+  console.log(result);
 };
 
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 
-if (!isNaN(arg1) && !isNaN(arg2)) {
-  console.log(add(arg1, arg2));
-} else {
-  // Print an error message
-  console.log('Invalid input. Please provide two integers.');
-}
+add(arg1, arg2));

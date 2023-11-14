65d33e91179fcdd6cfaf87ee5b3e60d581bdee97
alban-okoby <albanokoby225@gmail.com>
2023-11-14 15:31:31 -0800

    Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Function that returns the number of occurrences of a specific element in a list
+exports.nbOccurrences = function (list, searchElement) {
+  let count = 0;
+
+  for (const element of list) {
+    if (element === searchElement) {
+      count++;
+    }
+  }
+
+  return count;
+};

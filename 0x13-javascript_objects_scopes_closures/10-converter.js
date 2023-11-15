8f16a5836714e5590b0a386fe1969e303c537977
alban-okoby <albanokoby225@gmail.com>
2023-11-14 16:01:48 -0800

    issues

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  // Use the 'this' context to access the argument passed to the function
-  const convertedNumber = this.toString(base);
-  console.log(convertedNumber);
+  return function (num) {
+	    const convertedNumber = this.toString(base);
+	  return converterNumber;
+  };
 };
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-// Function that returns the number of occurrences of a specific element in a list
 exports.nbOccurrences = function (list, searchElement) {
   let count = 0;
-  for (const element of list) {
-    if (element === searchElement) {
+  for (let element = 0; element < list.length; element++) {
+    if (searchElement === list[element]) {
       count++;
     }
   }

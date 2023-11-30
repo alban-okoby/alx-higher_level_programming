3e56e469ab56658aedf501263f5a276e3e094d3e
alban-okoby <albanokoby225@gmail.com>
2023-11-30 10:57:18 -0800

    issue of body url & end url

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-curl -s "$1" | wc -c
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+response=$(curl -s -w "%{http_code}" "$1")
+http_status=$(tail -c 3 <<< "$response")
+
+if [ "$http_status" == "200" ]; then
+    # Display the body
+    body=$(head -c -3 <<< "$response")
+    echo "Response Body:"
+    echo "$body"
+else
+    echo "HTTP status code: $http_status"
+fi
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-curl -sL "$1"
+url="$1"
+
+# Check if URL is provided
+if [ -z "$url" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+response=$(curl -sL -w "%{http_code}" "$url")
+http_code=${response: -3}
+
+if [ "$http_code" = "200" ]; then
+    echo "${response::-3}"
+else
+    echo "Failed to retrieve response. HTTP status code: $http_code"
+    exit 1
+fi

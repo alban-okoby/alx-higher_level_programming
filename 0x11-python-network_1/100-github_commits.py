fb7b15865acacbd15d870a8c4939c360d89aa8a5
alban-okoby <albanokoby225@gmail.com>
2023-12-01 03:37:46 -0800

    ❌ FIxe issue of pycodestyle, line lenght

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,10 +8,10 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    repository_name = sys.argv[1]
+    repo_name = sys.argv[1]
     owner_name = sys.argv[2]
 
-    url = f'https://api.github.com/repos/{owner_name}/{repository_name}/commits'
+    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
     params = {'per_page': 10}
     response = requests.get(url, params=params)
 

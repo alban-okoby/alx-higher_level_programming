f534db9262171790627803cb9f048547c70e0dd3
alban-okoby <albanokoby225@gmail.com>
2024-01-03 15:03:43 -0800

    Issue of end 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,16 +2,4 @@
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(`Error occurred: ${error.message}`);
-  } else if (response.statusCode !== 200) {
-    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
-  } else {
-    fs.writeFileSync(filePath, body, 'utf-8');
-    console.log(`${filePath}`);
-  }
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));

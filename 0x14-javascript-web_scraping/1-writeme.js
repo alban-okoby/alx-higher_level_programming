b6d6dbbc80328537f0d38dfe70c6b507a039269d
alban-okoby <albanokoby225@gmail.com>
2024-01-03 14:57:32 -0800

    Fix Issue of 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-fs.promises.writeFile(filePath, contentToWrite, 'utf-8')
-  .then(() => console.log(`Content written to ${filePath}`))
-  .catch(error => console.error(`Error occurred: ${error.message}`));
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
